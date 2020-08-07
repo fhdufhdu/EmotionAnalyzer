53849bb2108ef1a5574ea0c1dba732468392c3c3
fhdufhdu <fhdufhdu@gmail.com>
2020-08-07 16:45:54 +0900

    20/08/07 16:45 commit Modified code

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import TrainData as td
 path = 'json/train_file_deform.json'
 json_dict = td.get_json(path)
 sentences = td.get_sentences(json_dict)
-td.tfidf_save(sentences)
 tfidf = td.tfidf_load()
 temp_data = td.get_train_data(json_dict, tfidf, sentences)
 train_data = temp_data[0]
@@ -17,8 +16,6 @@ predict_text = ['가격대비 마감허접',
                 '되게 좋네요',
                 '탁구제대로 치시려는분은 비추... 공이확실히 잘 안나가요']
 
-model = td.start_train(train_data, label_data, False)
-td.model_save(model)
 model = td.model_load()
 for text in predict_text:
     per = str(td.predict_data(model, tfidf, text)*100)
